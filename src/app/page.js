ab74a8afc3099d51b44f6444c49efc3638888451
developpper <98518184+developpper@users.noreply.github.com>
2025-04-26 22:51:05 +0400

    new

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import VideoSection from '@/components/ui/VideoSection';
+
+export default function Home() {
+  return (
+    <main className="w-full flex items-center justify-center">
+      <VideoSection />
+    </main>
+  );
+}
