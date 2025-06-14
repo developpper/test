acb17b9bbbc3873de487b26b324ef15c24a822f5
Vishvas Solanki <vishvas@artechnolabs.com>
2025-06-14 18:21:44 +0530

    feat(page): add /invoice route using Invoice component

---
+++ src/app/invoice/page.tsx
@@ -0,0 +1,12 @@
+// app/invoice/page.tsx
+
+import React from 'react';
+import Invoice from '../../components/ui/Invoice';
+
+export default function InvoicePage() {
+  return (
+    <main className="p-6">
+      <Invoice />
+    </main>
+  );
+}
